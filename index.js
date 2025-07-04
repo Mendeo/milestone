056370bf0c22cdb44bb3aec17ff745148911819b
Themen <thesolve@mail.ru>
2025-07-04 12:12:59 +0300

    Рабочая версия.

--- index.js
+++ index.js
@@ -5,47 +5,89 @@ const progressElement = document.querySelector('#progress .progress__line');
 const progressValueElement = document.querySelector('#progress .progress__line span');
 
 let endDate = localStorage.getItem('end_date');
+let startDate = localStorage.getItem('start_date');
 
-let time = 0;
-if (endDate)
+let endTime = 0;
+let startTime = 0;
+let timeAll = 0;
+let percent = 0;
+
+if (startDate && endDate)
 {
+	startDate = new Date(startDate);
 	endDate = new Date(endDate);
-	time = endDate.getTime() - Date.now();
+	startTime = startDate.getTime();
+	endTime = endDate.getTime();
+	timeAll = endTime - startTime;
+	percent = Math.floor((Date.now() - startTime) * 100 / timeAll, 1);
+
+	if (endTime - Date.now() < 0)
+	{
+		startNew();
+	}
+	else
+	{
+		updateEndDate(endDate);
+		updateProgress(percent);
+	}
 }
 else
+{
+	startNew();
+}
+
+update(timeAll);
+setInterval(() =>
+{
+	update(timeAll);
+}, 1000);
+
+function update(timeAll)
+{
+	let timePassed = Date.now() - startTime;
+	if (timePassed > timeAll)
+	{
+		startNew();
+		timePassed = Date.now() - startTime;
+	}
+	timerElement.innerText = msToString(timeAll - timePassed);
+	const currentPercent = timePassed * 100 / timeAll;
+	if (currentPercent - percent >= 0.1)
+	{
+		percent = Math.floor(currentPercent, 1);
+		updateProgress(percent);
+	}
+}
+function startNew()
 {
 	const week1 = 10000;//604800000;
 	const mean = 7 * week1 //7 недель.
 	const sigma = week1 / 1.644853627; //90% Интервал.
-	time = getNormalDistr(mean, sigma);
-	endDate = new Date(Date.now() + time);
-	//localStorage.setItem('end_date', endDate);
+
+	timeAll = getNormalDistr(mean, sigma);
+	timeAll = Math.floor(timeAll / 1000, 0) * 1000;
+	startTime = Date.now();
+	startTime = Math.floor(startTime / 1000, 0) * 1000;
+	endTime = startTime + timeAll;
+	startDate = new Date(startTime);
+	endDate = new Date(endTime);
+	percent = 0;
+	
+	updateEndDate(endDate);
+	updateProgress(percent);
+	
+	localStorage.setItem('start_date', startDate);
+	localStorage.setItem('end_date', endDate);
 }
-time = Math.floor(time / 1000) * 1000;
-
-endDateElement.innerText = endDate.toLocaleString('ru-RU', { weekday: 'long', year:'numeric', month:'long', day:'2-digit', hour:'2-digit', minute:'2-digit', second:'2-digit' });
-
-let timePassed = 0;
-let previousPercent = 0;
-update(time, timePassed);
-setInterval(() =>
+function updateEndDate(endDate)
 {
-	timePassed += 1000;
-	update(time, timePassed);
-}, 1000);
-
-function update(timeAll, timePassed)
-{
-	timerElement.innerText = msToString(timeAll - timePassed);
-	const currentPercent = timePassed * 100 / timeAll;
-	if (currentPercent - previousPercent >= 0.01)
-	{
-		previousPercent = Math.floor(currentPercent, 2);
-		progressElement.style.width = `${100 - previousPercent}%`;
-		progressValueElement.innerText = Math.floor(currentPercent, 1) + '%'
-	}
+	endDateElement.innerText = endDate.toLocaleString('ru-RU', { weekday: 'long', year:'numeric', month:'long', day:'2-digit', hour:'2-digit', minute:'2-digit', second:'2-digit' });
 }
-
+function updateProgress(percent)
+{
+	progressElement.style.width = `${100 - percent}%`;
+	progressValueElement.innerText = percent + '%'
+}
 function msToString(time)
 {
 	let aux = time;
@@ -58,7 +100,6 @@ function msToString(time)
 	const seconds = Math.floor(aux / 1000)
 	return `${to2digits(days)} : ${to2digits(hours)} : ${to2digits(minutes)} : ${to2digits(seconds)}`;
 }
-
 function to2digits(value)
 {
 	return value.toString().padStart(2, '0');
@@ -71,4 +112,3 @@ function getNormalDistr(mean, std)
 	if (rnd2 === 0) rnd2 = 1
 	return mean + Math.cos(2 * Math.PI * rnd1) * Math.sqrt(-2 * Math.log(rnd2)) * std;
 }
-
